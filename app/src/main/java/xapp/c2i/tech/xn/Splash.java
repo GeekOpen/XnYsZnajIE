219d1f637373d84125f01687154ae9325788561c
Geek Open <angeslikak@gmail.com>
2019-03-09 15:09:39 +0100

    Initial commit

---
+++ app/src/main/java/xapp/c2i/tech/xn/Splash.java
@@ -0,0 +1,13 @@
+package xapp.c2i.tech.xn;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
